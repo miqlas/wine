acc5984a61eee7f8af45d66039c5d2595d538987
Zoltán Mizsei <zmizsei@extrowerk.com>
2017-09-08 13:05:36 +0200

    ws2_32/socket.c patch

--- dlls/ws2_32/socket.c
+++ dlls/ws2_32/socket.c
@@ -907,7 +907,9 @@ static NTSTATUS sock_get_ntstatus( int err )
         case EDESTADDRREQ:      return STATUS_INVALID_PARAMETER;
         case EMSGSIZE:          return STATUS_BUFFER_OVERFLOW;
         case EPROTONOSUPPORT:
+        #ifndef __HAIKU__
         case ESOCKTNOSUPPORT:
+        #endif
         case EPFNOSUPPORT:
         case EAFNOSUPPORT:
         case EPROTOTYPE:        return STATUS_NOT_SUPPORTED;
@@ -952,7 +954,9 @@ static UINT sock_get_error( int err )
 	case EPROTOTYPE:	return WSAEPROTOTYPE;
 	case ENOPROTOOPT:	return WSAENOPROTOOPT;
 	case EPROTONOSUPPORT:	return WSAEPROTONOSUPPORT;
+	#ifndef __HAIKU__
 	case ESOCKTNOSUPPORT:	return WSAESOCKTNOSUPPORT;
+	#endif
 	case EOPNOTSUPP:	return WSAEOPNOTSUPP;
 	case EPFNOSUPPORT:	return WSAEPFNOSUPPORT;
 	case EAFNOSUPPORT:	return WSAEAFNOSUPPORT;
@@ -968,7 +972,9 @@ static UINT sock_get_error( int err )
 	case EISCONN:		return WSAEISCONN;
 	case ENOTCONN:		return WSAENOTCONN;
 	case ESHUTDOWN:		return WSAESHUTDOWN;
+	#ifndef __HAIKU__
 	case ETOOMANYREFS:	return WSAETOOMANYREFS;
+	#endif
 	case ETIMEDOUT:		return WSAETIMEDOUT;
 	case ECONNREFUSED:	return WSAECONNREFUSED;
 	case ELOOP:		return WSAELOOP;
